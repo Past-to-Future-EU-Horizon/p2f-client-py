8605090c114117ad8788419bd295ae16fd3170f6
Garrett Speed <g.t.speed@uu.nl>
2025-11-20 21:02:32 +0100

    Update methods in datasets.py to have more natural return methods

--- p2f_client/datasets.py
+++ p2f_client/datasets.py
@@ -12,16 +12,17 @@ class datasets:
         self.base_url = furl(base_url)
         self.prefix = "datasets"
         self.dataset_url = self.base_url / self.prefix
-        self.datasets = []
+        self.upload_queue = []
     def add_dataset(self, dataset: Datasets):
         self.datasets.append(dataset)
     def upload_datasets(self):
         uploaded_datasets = []
-        for dataset in self.datasets:
+        for dataset in self.upload_queue:
             r = requests.post(self.dataset_url,
                               data=dataset.model_dump_json(exclude_unset=True))
             uploaded_datasets.append(Datasets(**r.json()))
-        self.uploaded_datasets = uploaded_datasets
+        # self.uploaded_datasets = uploaded_datasets
+        return uploaded_datasets
     def upload_dataset(self, dataset: Datasets):
         r = requests.post(self.dataset_url,
                           data=dataset.model_dump_json(exclude_unset=True))
@@ -32,19 +33,20 @@ class datasets:
                              doi: Optional[str]=None) -> List[Datasets]:
         list_url = self.dataset_url
         if is_new_p2f:
-            list_url["is_new_p2f"] = is_new_p2f
+            list_url.args["is_new_p2f"] = is_new_p2f
         if is_sub_dataset:
-            list_url["is_sub_dataset"] = is_sub_dataset
+            list_url.args["is_sub_dataset"] = is_sub_dataset
         if doi:
-            list_url["doi"] = doi
+            list_url.args["doi"] = doi
         r = requests.get(list_url)
-        self.datasets = [Datasets(**x) for x in r.json()]
+        # self.datasets = [Datasets(**x) for x in r.json()]
+        return [Datasets(**x) for x in r.json()]
     def get_remote_dataset(self, dataset_id):
         get_url = self.dataset_url
-        get_url["dataset_id"] = dataset_id
+        get_url.args["dataset_id"] = dataset_id
         r = requests.get(get_url)
         return Datasets(**r.json())
     def delete_remote_dataset(self, dataset_id):
         delete_url = self.dataset_url
-        delete_url["dataset_id"] = dataset_id
+        delete_url.args["dataset_id"] = dataset_id
         r = requests.delete(delete_url)
