650f8768b84eb6c87259754bef705956b5d47ec4
Garrett Speed <g.t.speed@uu.nl>
2026-04-20 12:29:24 +0200

    Add API Endpoint metadata check

--- p2f_client/p2f_client.py
+++ p2f_client/p2f_client.py
@@ -9,6 +9,7 @@ from .harm_timeslice import harm_timeslice
 from .harm_reference import harm_reference
 from .conn import health_check
 from p2f_pydantic.temp_accounts import Temp_Account
+from p2f_pydantic.system import API_Metadata
 # Third Party Libraries
 import requests
 import furl
@@ -26,7 +27,7 @@ class P2F_Client:
                  email: Optional[str]=None, 
                  token: Optional[str] = None, 
                  token_expiration: Optional[datetime]=None):
-        self.version = (0, 0, 10) # turn this into a real named tuple one day
+        self.version = (0, 0, 13) # turn this into a real named tuple one day
         self.hostname = hostname
         self.port = port
         if https:
@@ -47,6 +48,20 @@ class P2F_Client:
             if self.email is not None:
                 self.temp_account = Temp_Account(email=self.email, token=self.token)
         self.child_class_loading()
+        self.probe_api_endpoint()
+    def probe_api_endpoint(self):
+        if health_check(self.base_url):
+            r = requests.get(self.base_url / "version")
+            if r.ok:
+                api_meta = API_Metadata(**r.json())
+                if api_meta.pyclient_minimum_version.major > self.version[0]:
+                    raise EnvironmentError("Current p2f-client-py version does not match API server's minimum supported version. Update your p2f-client-py library.")
+                else:
+                    if api_meta.pyclient_minimum_version.minor > self.version[1]:
+                        raise RuntimeWarning("The minor version of this library, p2f-client-py, is less than the minimum supported minor version of the API server. You may experience errors when interacting with the API through the client. Please update p2f-client-py")
+                    else:
+                        if api_meta.pyclient_minimum_version.patch > self.version[2]:
+                            raise RuntimeWarning("The patch version of this library, p2f-client-py, is less than the minimum supported patch version of the API server. You should not, but may experience errors when interacting with the API through the client. Please update p2f-client-py")
     def child_class_loading(self):
         # Separated this out so we can reload it later. 
         self.datasets = datasets(self)
